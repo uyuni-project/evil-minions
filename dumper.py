91f714b05a91ad3edebbf6a6470c3ec5cf0929a9
Silvio Moioli <smoioli@suse.de>
2017-04-27 15:24:51 +0200

    Refactor Dumper in a separate class

---
+++ dumper.py
@@ -0,0 +1,47 @@
+import yaml
+import time
+
+import tornado.gen
+
+from salt.transport.zeromq import AsyncZeroMQReqChannel
+from salt.transport.zeromq import AsyncZeroMQPubChannel
+
+class Dumper(object):
+    '''Monkey-patches ZeroMQ core I/O classes to dump exchanged messages to a Yaml file.'''
+    def __init__(self, trace_path):
+        self.dump_file = open(trace_path, 'w+')
+
+    def install(self):
+        AsyncZeroMQReqChannel.dump = self.dump
+        AsyncZeroMQReqChannel.send_original = AsyncZeroMQReqChannel.send
+        AsyncZeroMQReqChannel.send = logging_send
+
+        AsyncZeroMQPubChannel.dump = self.dump
+        AsyncZeroMQPubChannel.on_recv_original = AsyncZeroMQPubChannel.on_recv
+        AsyncZeroMQPubChannel.on_recv = logging_on_recv
+
+    def dump(self, load, socket):
+        header = {
+            'socket' : socket,
+            'time' : time.time(),
+        }
+        event = {
+            'header' : header,
+            'load' : load,
+        }
+        self.dump_file.write("---\n")
+        self.dump_file.write(yaml.safe_dump(event, default_flow_style=False))
+        self.dump_file.flush()
+
+@tornado.gen.coroutine
+def logging_send(self, load, **kwargs):
+    self.dump(load, 'REQ')
+    ret = yield self.send_original(load, **kwargs)
+    self.dump(ret, 'REP')
+    raise tornado.gen.Return(ret)
+
+def logging_on_recv(self, callback):
+    def logging_callback(load):
+        self.dump(load, 'PUB')
+        callback(load)
+    return self.on_recv_original(logging_callback)
