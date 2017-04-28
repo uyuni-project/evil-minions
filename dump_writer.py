d8609c6994dd758a8f97a9ffb17e5631e1438e0f
Silvio Moioli <smoioli@suse.de>
2017-04-28 13:05:29 +0200

    DumpWtriter: more method renaming and documentation comments for clarity

--- dump_writer.py
+++ dump_writer.py
@@ -7,20 +7,23 @@ from salt.transport.zeromq import AsyncZeroMQReqChannel
 from salt.transport.zeromq import AsyncZeroMQPubChannel
 
 class DumpWriter(object):
-    '''Monkey-patches ZeroMQ core I/O classes to dump exchanged messages to a Yaml file.'''
+    '''Dumps ZeroMQ I/O messages into a Yaml file.'''
+
     def __init__(self, trace_path):
         self.dump_file = open(trace_path, 'w+')
 
-    def install(self):
-        AsyncZeroMQReqChannel.dump = self.dump
+    def attach(self):
+        '''Monkey-patches ZeroMQ core I/O classes to dump exchanged messages to a Yaml file.'''
+        AsyncZeroMQReqChannel._dump = self._dump
         AsyncZeroMQReqChannel.send_original = AsyncZeroMQReqChannel.send
         AsyncZeroMQReqChannel.send = _logging_send
 
-        AsyncZeroMQPubChannel.dump = self.dump
+        AsyncZeroMQPubChannel._dump = self._dump
         AsyncZeroMQPubChannel.on_recv_original = AsyncZeroMQPubChannel.on_recv
         AsyncZeroMQPubChannel.on_recv = _logging_on_recv
 
-    def dump(self, load, socket):
+    def _dump(self, load, socket):
+        '''Dumps a ZeroMQ message, socket identifies the channel and direction'''
         header = {
             'socket' : socket,
             'time' : time.time(),
@@ -35,13 +38,13 @@ class DumpWriter(object):
 
 @tornado.gen.coroutine
 def _logging_send(self, load, **kwargs):
-    self.dump(load, 'REQ')
+    self._dump(load, 'REQ')
     ret = yield self.send_original(load, **kwargs)
-    self.dump(ret, 'REP')
+    self._dump(ret, 'REP')
     raise tornado.gen.Return(ret)
 
 def _logging_on_recv(self, callback):
     def _logging_callback(load):
-        self.dump(load, 'PUB')
+        self._dump(load, 'PUB')
         callback(load)
     return self.on_recv_original(_logging_callback)
