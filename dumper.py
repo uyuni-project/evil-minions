eea0ed76f404e0d5cd51809b41d4ca20425e893e
Silvio Moioli <smoioli@suse.de>
2017-04-28 12:46:10 +0200

    Dumper: refactoring: internal methods renamed

--- dumper.py
+++ dumper.py
@@ -14,11 +14,11 @@ class Dumper(object):
     def install(self):
         AsyncZeroMQReqChannel.dump = self.dump
         AsyncZeroMQReqChannel.send_original = AsyncZeroMQReqChannel.send
-        AsyncZeroMQReqChannel.send = logging_send
+        AsyncZeroMQReqChannel.send = _logging_send
 
         AsyncZeroMQPubChannel.dump = self.dump
         AsyncZeroMQPubChannel.on_recv_original = AsyncZeroMQPubChannel.on_recv
-        AsyncZeroMQPubChannel.on_recv = logging_on_recv
+        AsyncZeroMQPubChannel.on_recv = _logging_on_recv
 
     def dump(self, load, socket):
         header = {
@@ -34,14 +34,14 @@ class Dumper(object):
         self.dump_file.flush()
 
 @tornado.gen.coroutine
-def logging_send(self, load, **kwargs):
+def _logging_send(self, load, **kwargs):
     self.dump(load, 'REQ')
     ret = yield self.send_original(load, **kwargs)
     self.dump(ret, 'REP')
     raise tornado.gen.Return(ret)
 
-def logging_on_recv(self, callback):
-    def logging_callback(load):
+def _logging_on_recv(self, callback):
+    def _logging_callback(load):
         self.dump(load, 'PUB')
         callback(load)
-    return self.on_recv_original(logging_callback)
+    return self.on_recv_original(_logging_callback)
